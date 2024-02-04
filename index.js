5b779d2e825800915df5cb08d18c762db0c46e4d
Ramon Gonzalez Casillas <56400522+Mosquiscript@users.noreply.github.com>
2024-02-04 04:25:41 -0600

    updste

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const tabla = document.getElementById("tabla");
 
 let row = tabla.insertRow(-1);
 let celdasMax = 24;
-let celda = row.insertCell(0);
 
-celda.innerText = "hola";
 
 for (let index = 0; index < celdasMax; index++) {
       
-      
+      let celda = row.insertCell(index);
+      celda.innerText = "1";
 }
