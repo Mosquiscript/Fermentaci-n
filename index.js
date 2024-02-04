013c1a8e72fb44366143775fc75d41d1f5e5913d
Ramon Gonzalez Casillas <56400522+Mosquiscript@users.noreply.github.com>
2024-02-04 05:40:11 -0600

    jfjjf

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const tabla = document.getElementById("tabla");
+function generateTable() {
+        // creates a <table> element and a <tbody> element
+          const tbl = document.createElement("table");
+            const tblBody = document.createElement("tbody");
 
-let row = tabla.insertRow(-1);
-let celdasMax = 24;
+              // creating all cells
+                for (let i = 0; i < 2; i++) {
+                    // creates a table row
+                        const row = document.createElement("tr");
 
+                            for (let j = 0; j < 2; j++) {
+                                  // Create a <td> element and a text node, make the text
+                                        // node the contents of the <td>, and put the <td> at
+                                              // the end of the table row
+                                                    const cell = document.createElement("td");
+                                                          const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+                                                                cell.appendChild(cellText);
+                                                                      row.appendChild(cell);
+                                                                          }
 
-for (let index = 0; index < celdasMax; index++) {
-      
-      let celda = row.insertCell(index);
-      celda.innerText = "1";
+                                                                              // add the row to the end of the table body
+                                                                                  tblBody.appendChild(row);
+                                                                                    }
+
+                                                                                      // put the <tbody> in the <table>
+                                                                                        tbl.appendChild(tblBody);
+                                                                                          // appends <table> into <body>
+                                                                                            document.body.appendChild(tbl);
+                                                                                              // sets the border attribute of tbl to '2'
+                                                                                                tbl.setAttribute("border", "2");
+                                                                                                }
+                                                                                                
 }
