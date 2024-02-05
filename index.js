74eebe1aca51b900fac1c8de95f88da3e2b8a1d7
Ramon Gonzalez Casillas <56400522+Mosquiscript@users.noreply.github.com>
2024-02-04 21:36:40 -0600

    update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const tabla = getElementById("tabla");
+function addRow(tableID) {
+    // Obtiene una referencia a la tabla
+    var tableRef = document.getElementById(tableID);
 
-for(let i=0; i< 24; i++){
-      tabla.innerHTML +=  
-      `   
-          <tr>
-              <td>${i}</td>
-             
-          </tr>
-      `
-}
+    // Inserta una fila en la tabla, en el índice 0
+    var newRow = tableRef.insertRow(0);
+
+    // Inserta una celda en la fila, en el índice 0
+    var newCell = newRow.insertCell(0);
+
+    // Añade un nodo de texto a la celda
+    var newText = document.createTextNode("Nueva fila superior");
+    newCell.appendChild(newText);
+  }
+
+  // Llama a addRow() con el ID de la tabla
+  addRow("tabla");
